3b0b7078df3ba00efc95d8c72c11661df84bc5ad
Jacin <jacinjiyan@gmail.com>
2021-03-25 02:01:39 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const { readdirSync } = require('fs')
+const express = require('express')
+const morgan = require('morgan')
+const connectDB = require('./db')
+const cors = require('cors')
+
+const app = express()
+
+require('dotenv').config({ path: '.env' })
+
+connectDB()
+
+app.use(cors())
+app.use(morgan('dev'))
+
+// dynamic routing
+readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)))
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is running on port ${port}`);
+    }
+})
