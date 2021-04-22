43758b459c4f3f656f9f59577d9ef7e51af63e3e
Jacin <jacinjiyan@gmail.com>
2021-04-23 08:13:37 +1000

    modified env routing

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
+const itemRoutes = require ('./routes/productRoutes.js')
+const userRoutes = require ('./routes/userRoutes.js')
+const orderRoutes = require ('./routes/orderRoutes.js')
+const uploadRoutes = require ('./routes/uploadRoutes.js')
+
 require("dotenv").config({ path: ".env" });
 require("colors");
 
@@ -18,10 +23,19 @@ connectDB();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-// dynamic routing
-readdirSync(path.join(__dirname, "routes")).forEach((fileName) =>
-  app.use("/api", require(path.join(__dirname, "routes") + `/${fileName}`))
-);
+
+if (process.env.NODE_ENV === "development") {
+  // dynamic routing
+  readdirSync(path.join(__dirname, "routes")).forEach((fileName) =>
+    app.use("/api", require(path.join(__dirname, "routes") + `/${fileName}`))
+  );
+} else {
+  app.use('api/', itemRoutes)
+  app.use('api/', userRoutes)
+  app.use('api/', orderRoutes)
+  app.use('api/', uploadRoutes)
+}
+
 //paypal
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
