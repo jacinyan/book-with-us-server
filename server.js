f193614401e7af70c235c6574f05e2ca4f3c5beb
Jacin <jacinjiyan@gmail.com>
2021-04-23 08:43:12 +1000

    no major issues found

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
-const itemRoutes = require ('./routes/itemRoutes.js')
-const userRoutes = require ('./routes/userRoutes.js')
-const orderRoutes = require ('./routes/orderRoutes.js')
-const uploadRoutes = require ('./routes/uploadRoutes.js')
 
 require("dotenv").config({ path: ".env" });
 require("colors");
@@ -24,17 +20,9 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "development") {
-  // dynamic routing
-  readdirSync(path.join(__dirname, "routes")).forEach((fileName) =>
+readdirSync(path.join(__dirname, "routes")).forEach((fileName) =>
     app.use("/api", require(path.join(__dirname, "routes") + `/${fileName}`))
   );
-} else {
-  app.use('/api', itemRoutes)
-  app.use('/api', userRoutes)
-  app.use('/api', orderRoutes)
-  app.use('/api', uploadRoutes)
-}
 
 //paypal
 app.get("/api/config/paypal", (req, res) =>
